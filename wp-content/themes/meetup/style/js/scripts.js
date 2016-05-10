940b2c6f5eb3fa721d120bd2b330e07dd07ac757
Milan Jovanovic <milan@novarepublika.com>
2016-05-10 18:11:25 +0200

    Added RedBull tracking code

--- wp-content/themes/meetup/style/js/scripts.js
+++ wp-content/themes/meetup/style/js/scripts.js
@@ -250,4 +250,8 @@ jQuery(window).load(function() {
         organizationElInput.attr('disabled', 'disabled');
       }
     });
+    var redBullUrl = "http://redbull.rs";
+    var redBullTitle = "FabLab";
+    jQuery('a[href="'+ redBullUrl +'"]').attr('title', redBullTitle);
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-46080369-3', 'auto', {'name': 'eventwidgetTracker'});ga('eventwidgetTracker.set', 'anonymizeIp', true);ga('eventwidgetTracker.set', 'dimension1', 'FabLab');ga('eventwidgetTracker.send', 'pageview');
 }); 
